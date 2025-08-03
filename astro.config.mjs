9059eaa5ab8a2904886ef3982f29f0031c5bc531
Luis Lopez <l.lopezperdomo.e@gmail.com>
2025-08-03 14:15:51 -0300

    Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,22 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lexia.netlify.app', // Cambia esto por tu dominio de Netlify
+  output: 'static',
+  build: {
+    format: 'directory'
+  },
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    build: {
+      cssCodeSplit: true,
+      rollupOptions: {
+        output: {
+          assetFileNames: 'assets/[name].[hash][extname]',
+          chunkFileNames: 'assets/[name].[hash].js',
+          entryFileNames: 'assets/[name].[hash].js'
+        }
+      }
+    }
   }
 });
